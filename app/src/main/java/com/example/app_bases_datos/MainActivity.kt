2c985f83918e650af6f6db935756b4eb675e9d71
Diego Fernandez <diegoa.fernandezr@utadeo.edu.co>
2024-10-21 17:07:19 -0500

    Navegación completa de tutorial a mainActivity con Login, register y conexion con secciones con Nav Buttom
    Consideración:
    Falta implementar back button correctamente en fragmentPerfil a mainActivity, asi como botones de retroceder en cada uno de las partes.

--- app/src/main/java/com/example/app_bases_datos/MainActivity.kt
+++ app/src/main/java/com/example/app_bases_datos/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.app_bases_datos.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -15,34 +17,48 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private val auth: FirebaseAuth = Firebase.auth
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val logoutBtn = findViewById<Button>(R.id.logout)
         val user = auth.currentUser
-        val info = findViewById<TextView>(R.id.texto)
 
         if (user == null) {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
             finish()
         } else {
-            info.text = user.email
+            replaceFragment(Buscar())
+            binding.bottomNavigationView.setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.buscar -> replaceFragment(Buscar())
+                    R.id.rutas -> replaceFragment(Rutas())
+                    R.id.perfil -> replaceFragment(Perfil())
+                }
+                true
+            }
         }
 
-        logoutBtn.setOnClickListener {
-            auth.signOut()
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish()
-        }
+
     }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/app_bases_datos/Perfil.kt
@@ -0,0 +1,43 @@
+package com.example.app_bases_datos
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class Perfil : Fragment() {
+
+    private val auth: FirebaseAuth = Firebase.auth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_perfil, container, false)
+
+        val logoutBtn = root.findViewById<Button>(R.id.logout)
+        val user = auth.currentUser
+        val info = root.findViewById<TextView>(R.id.texto)
+
+        info.text = user!!.email
+
+        logoutBtn.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(activity, Login::class.java)
+            startActivity(intent)
+
+        }
+
+        return root
+    }
+
+}
