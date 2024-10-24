8884769a5506f41100034ad74d9a16b2c7c94afb
Diego <deigoalejo05@gmail.com>
2024-10-24 11:28:16 -0500

    ahora si

--- app/src/main/java/com/example/app_bases_datos/MainActivity.kt
+++ app/src/main/java/com/example/app_bases_datos/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.app_bases_datos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.app_bases_datos.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-
-    private val auth: FirebaseAuth = Firebase.auth
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,35 +22,21 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val user = auth.currentUser
-
-        if (user == null) {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish()
-        } else {
-            replaceFragment(Buscar())
-            binding.bottomNavigationView.setOnItemSelectedListener {
-                when(it.itemId){
-                    R.id.buscar -> replaceFragment(Buscar())
-                    R.id.rutas -> replaceFragment(Rutas())
-                    R.id.perfil -> replaceFragment(Perfil())
-                }
-                true
+        replaceFragment(Dos_mitades())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.dos_mitades -> replaceFragment(Dos_mitades())
+                R.id.dos_palabras -> replaceFragment(Dos_palabras())
+                R.id.quitar_fragmento -> replaceFragment(Quitar_fragemento())
             }
+            true
         }
-
-
     }
-
     private fun replaceFragment(fragment : Fragment){
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
-
-
     }
-
 }
