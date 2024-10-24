2ee13dc7bd658e52d77e013137a9d527bc81e792
Diego <deigoalejo05@gmail.com>
2024-10-24 12:42:35 -0500

    Se diseño el hola

--- app/src/main/java/com/example/app_bases_datos/Login.kt
+++ app/src/main/java/com/example/app_bases_datos/Login.kt
@@ -2,7 +2,9 @@ package com.example.app_bases_datos
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
     private lateinit var loginBtn: Button
+    private lateinit var emailInput: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,11 +22,15 @@ class Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        emailInput = findViewById(R.id.Usuario)
         loginBtn = findViewById(R.id.loginBtn)
+
         loginBtn.setOnClickListener {
-            Intent(this, MainActivity::class.java).also {
-                startActivity(it)
+            val email = emailInput.text.toString()
+            Log.d("LoginActivity", "Correo a pasar: $email") //
+            Intent(this, MainActivity::class.java).also { intent ->
+                intent.putExtra("EMAIL", email)
+                startActivity(intent)
                 finish()
             }
         }
