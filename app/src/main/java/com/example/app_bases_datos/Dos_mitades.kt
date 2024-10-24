ba4c2627001e4e5da852f709f18692b90b0db8d2
saragap1 <saraangel119@gmail.com>
2024-10-24 12:07:36 -0500

    DOS MITADES

--- app/src/main/java/com/example/app_bases_datos/Dos_mitades.kt
+++ app/src/main/java/com/example/app_bases_datos/Dos_mitades.kt
@@ -1,20 +1,53 @@
 package com.example.app_bases_datos
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 class Dos_mitades : Fragment() {
 
+    private lateinit var boton: Button
+    private lateinit var inputCadena: EditText
+    private lateinit var resultado: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dos_mitades, container, false)
+        val view = inflater.inflate(R.layout.fragment_dos_mitades, container, false)
+
+        boton = view.findViewById(R.id.btn)
+        inputCadena = view.findViewById(R.id.editText)
+        resultado = view.findViewById(R.id.textView7)
+
+        boton.setOnClickListener {
+            val cadena = inputCadena.text.toString()
+
+            if (cadena.isNotBlank()) {
+                val resultadoTexto = intercambiarMitades(cadena)
+                resultado.text = resultadoTexto
+            } else {
+                resultado.text = "Ups! Algo salió mal. Revisa tu cadena"
+            }
+        }
+        return view
     }
 
+    private fun intercambiarMitades(cadena: String): String {
+        val longitud = cadena.length
+        val puntoCorte = (longitud + 1) / 2 // +1 para el caso impar
+
+        val primeraMitad = cadena.substring(0, puntoCorte)
+        val segundaMitad = cadena.substring(puntoCorte)
+
+        return segundaMitad + primeraMitad
+    }
 }
+
