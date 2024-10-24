b75a1a5f76044d4037c3f315713500aab8ecdae6
Diego <deigoalejo05@gmail.com>
2024-10-24 11:55:14 -0500

    Jeje ultimo push

--- app/src/main/java/com/example/app_bases_datos/Login.kt
+++ app/src/main/java/com/example/app_bases_datos/Login.kt
@@ -8,14 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Start : AppCompatActivity() {
-
+class Login : AppCompatActivity() {
     private lateinit var loginBtn: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,9 +21,8 @@ class Start : AppCompatActivity() {
         }
 
         loginBtn = findViewById(R.id.loginBtn)
-
         loginBtn.setOnClickListener {
-            Intent(this, Login::class.java).also {
+            Intent(this, MainActivity::class.java).also {
                 startActivity(it)
                 finish()
             }
