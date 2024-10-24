4793c9dd6f9ef2ead65efd5e184b2ee65a758ad8
Diego <deigoalejo05@gmail.com>
2024-10-24 12:24:04 -0500

    Funciona 2 funciones

--- app/src/main/java/com/example/app_bases_datos/Dos_mitades.kt
+++ app/src/main/java/com/example/app_bases_datos/Dos_mitades.kt
@@ -1,6 +1,5 @@
 package com.example.app_bases_datos
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/com/example/app_bases_datos/Quitar_fragmento.kt
+++ app/src/main/java/com/example/app_bases_datos/Quitar_fragmento.kt
@@ -5,21 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 
 class Quitar_fragmento : Fragment() {
-
+    private lateinit var inputField: EditText
+    private lateinit var resultTextView: TextView
+    private lateinit var processButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_quitar_framento, container, false)
-        val info = root.findViewById<TextView>(R.id.texto)
+        val view = inflater.inflate(R.layout.fragment_quitar_framento, container, false)
 
+        inputField = view.findViewById(R.id.inputField)
+        resultTextView = view.findViewById(R.id.resultTextView)
+        processButton = view.findViewById(R.id.processButton)
 
-        return root
+        processButton.setOnClickListener {
+            val input = inputField.text.toString()
+            resultTextView.text = removeH(input)
+        }
+        return view
+    }
+
+    private fun removeH(input: String): String {
+        val firstHIndex = input.indexOf('h')
+        val lastHIndex = input.lastIndexOf('h')
+
+        // Validamos que haya al menos dos 'h'
+        return if (firstHIndex != -1 && lastHIndex != -1 && firstHIndex != lastHIndex) {
+            input.substring(0, firstHIndex) + input.substring(lastHIndex + 1)
+        } else {
+            "error"
+        }
     }
 
 }
