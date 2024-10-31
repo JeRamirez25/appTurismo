8a10dca9d4a0105b352954537160db2ab0f33bff
saragap1 <saraangel119@gmail.com>
2024-10-31 11:12:13 -0500

    Cuando el usuario esta logueado, el tutorial no aparece

--- app/src/main/java/com/example/app_bases_datos/MainActivityTutorial.kt
+++ app/src/main/java/com/example/app_bases_datos/MainActivityTutorial.kt
@@ -1,13 +1,19 @@
 package com.example.app_bases_datos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivityTutorial : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val auth: FirebaseAuth = Firebase.auth
+
+override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_tutorial)
@@ -15,6 +21,13 @@ class MainActivityTutorial : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+            val user = auth.currentUser
+
+            if (user != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
         }
     }
 }
