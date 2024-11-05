24a4795e34f05ced34230b9091a6f61562307321
Axel <kevinaxelhr18@gmail.com>
2024-11-05 13:42:24 -0500

    Ya se asoció el usuario con la verificacion en las rutas

--- app/src/main/java/com/example/app_bases_datos/Rutas.kt
+++ app/src/main/java/com/example/app_bases_datos/Rutas.kt
@@ -8,34 +8,42 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.example.app_bases_datos.utils.obtenerIdUsuario
 import com.example.app_bases_datos.utils.verificarRuta
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
 class Rutas : Fragment() {
-
+    private val auth: FirebaseAuth = Firebase.auth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val ID_USUARIO = "qDKCX43GUHbA0pyB8yRl"
         var root: View? = null
-
-        lifecycleScope.launch {
-            val tieneRutas = verificarRuta(ID_USUARIO)
-            Log.d("Firestore", "¿El usuario tiene rutas? $tieneRutas")
-            if (tieneRutas) {
-                val fragmentManager = getFragmentManager()
-                val fragmentTransaction = fragmentManager?.beginTransaction()
-                if (fragmentTransaction != null) {
-                    fragmentTransaction.replace(R.id.frame_layout, RutasHome())
-                    fragmentTransaction.commit()
-                }
-            } else {
-                val fragmentManager = getFragmentManager()
-                val fragmentTransaction = fragmentManager?.beginTransaction()
-                if (fragmentTransaction != null) {
-                    fragmentTransaction.replace(R.id.frame_layout, NoRutas())
-                    fragmentTransaction.commit()
+        val user = auth.currentUser
+        if (user != null) {
+            val userEmail = user.email ?: ""
+            obtenerIdUsuario(userEmail) { ID_USUARIO ->
+                lifecycleScope.launch {
+                    val tieneRutas = ID_USUARIO?.let { verificarRuta(it) }
+                    Log.d("Firestore", "¿El usuario tiene rutas? $tieneRutas")
+                    if (tieneRutas == true) {
+                        val fragmentManager = getFragmentManager()
+                        val fragmentTransaction = fragmentManager?.beginTransaction()
+                        if (fragmentTransaction != null) {
+                            fragmentTransaction.replace(R.id.frame_layout, RutasHome())
+                            fragmentTransaction.commit()
+                        }
+                    } else {
+                        val fragmentManager = getFragmentManager()
+                        val fragmentTransaction = fragmentManager?.beginTransaction()
+                        if (fragmentTransaction != null) {
+                            fragmentTransaction.replace(R.id.frame_layout, NoRutas())
+                            fragmentTransaction.commit()
+                        }
+                    }
                 }
             }
         }
